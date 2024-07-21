e5bdd4033a3624b7999b198f67337858620b9764
Maxwell <120192706+VLazorkite@users.noreply.github.com>
2024-07-21 21:58:00 +0100

    About and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,36 @@
+import About from "@/components/About";
+import Card from "@/components/Card";
+import TopSection from "@/components/TopSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="Top layer">
-        <h1>Maxwell Rotich</h1>
+      <main className="container">
+        <TopSection />
 
-        <ul className="social links">
-          <li>
-            <a href="#">github</a>
-          </li>
-          <li>
-            <a href="#">linkedIn</a>
-          </li>
-          <li>
-            <a href="#">twitter</a>
-          </li>
-        </ul>
+        <About />
 
-        <h2>
-          <li>Me</li>
-        </h2>
-        <h2>
-          <li>Work</li>
-        </h2>
-        <h2>
-          <li>Blog</li>
-        </h2>
-      </div>
-
-      <div className="Bio">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,
-          voluptas aspernatur? Perspiciatis aliquid deserunt quaerat
-          reprehenderit placeat nobis? Cumque quasi rerum placeat nesciunt
-          praesentium doloribus nulla laboriosam suscipit blanditiis aliquam.
-        </p>
-      </div>
-
-      <div className="work grid grid-cols-4 gap-4">
-        <div className="design">
-          <div>Project 1</div>
-          <div>Project 2</div>
-          <div>Project 3</div>
-        </div>
-      </div>
-
-      <div className="Blog">
-        <a href="/Blog">
-          <div className="design">
-            <li>Blog</li>
+        <div className="work bg-red-500">
+          <div className="design grid grid-cols-4 gap-4">
+            <Card />
+            <Card />
+            <Card />
           </div>
-        </a>
-      </div>
+        </div>
 
-      <div className="Philanthropy and interests">
-        <div className="design">content</div>
-      </div>
+        <div className="Blog">
+          <a href="/Blog">
+            <div className="design">
+              <li>Blog</li>
+            </div>
+          </a>
+        </div>
+
+        <div className="Philanthropy and interests">
+          <div className="design">content</div>
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/components/TopSection.tsx
@@ -0,0 +1,35 @@
+export default function TopSection() {
+  return (
+    <div className="Top layer w-ful h-60  ">
+      <header className="Title flex ">
+        <div className="Name h-full">
+          <div className="Name-split">
+            <div>Maxwell</div>
+          </div>
+
+          <div className="Name-split">
+            <div>Rotich</div>
+          </div>
+        </div>
+        <div className="About flex space-x-10 list-none  ">
+          <h2>
+            I'm a backend developer with a keen interest in Neural networks and
+            ML algorithims. Enthusiastic learner and problem solver. 3rd Year
+            Computer science and Math student.
+          </h2>
+        </div>
+      </header>
+      <ul className="Social-links  flex space-x-4">
+        <li>
+          <a href="#">github</a>
+        </li>
+        <li>
+          <a href="#">linkedIn</a>
+        </li>
+        <li>
+          <a href="#">twitter</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
